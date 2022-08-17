710307e7e9e736e0a744b536d4499a889127445a
Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
2022-08-17 00:19:28 -0300

    fix: hashc strategy and update insertion

--- src/grokking/hash_table.c
+++ src/grokking/hash_table.c
@@ -5,26 +5,30 @@ int main(int argc, char *argv) {
   int hashpos = 0;
   char hash_buffer_c[MAX_KEYWORD_INPUT];
 
-  hashpos = hashc(hash_buffer_c);
-
   init_hash_table(hash_table);
 
   while (hash_buffer_c != "\\q") {
     scanf("%s", hash_buffer_c);
+    hashpos = hashc(hash_buffer_c);
     insert_hash_table(hash_table, hashpos, hash_buffer_c);
     pretty_print_table(hash_table);
-    hash_buffer_c[0] = '\0';
+    hash_buffer_c[0] = 0;
   }
 
   return EXIT_SUCCESS;
 }
 
+void strswap(char *left, char *right) {
+  for (int i = 0; i < MAX_KEYWORD_INPUT; i++) {
+    left[i] = right[i];
+  }
+}
+
 int hashc(char *keyword) {
   int buffer = 0;
-  int keyword_length = sizeof(keyword) / sizeof(keyword[0]);
 
-  for (int i = 0; i < keyword_length; i++) {
-    buffer += ((int)keyword[i]) % MAX_TABLE_SIZE;
+  for (int i = 0; i < MAX_KEYWORD_INPUT; i++) {
+    buffer += keyword[i] % 11;
   }
 
   return buffer % MAX_TABLE_SIZE;
@@ -33,27 +37,25 @@ int hashc(char *keyword) {
 void init_hash_table(Data **hash_table) {
   for (int i = 0; i < MAX_TABLE_SIZE; i++) {
     hash_table[i] = malloc(sizeof(Data));
-    (*hash_table[i]).name = "\0";
+    hash_table[i]->name = malloc(sizeof(char) * MAX_KEYWORD_INPUT);
     (*hash_table[i]).pNext = NULL;
   }
 }
 
 void insert_hash_table(Data **hash_table, int hash, char *name) {
-  Data *new_data = malloc(sizeof(Data));
-  new_data->name = name;
-  new_data->pNext = NULL;
-
   Data *hash_line = hash_table[hash];
+
   while (hash_line) {
-    if (hash_line->name == "\0") {
-      hash_line->name = name;
+    if (hash_line->name[0] == '\0') {
+      strswap(hash_line->name, name);
       break;
-    } else if (!hash_line->pNext) {
+    } else {
       hash_line->pNext = malloc(sizeof(Data));
-      hash_line->pNext->name = name;
-      hash_line->pNext->pNext = NULL;
+      hash_line->pNext->name = malloc(sizeof(char) * MAX_KEYWORD_INPUT);
+      strswap(hash_line->pNext->name, name);
       break;
     }
+
     hash_line = hash_line->pNext;
   }
 }
@@ -67,7 +69,7 @@ void pretty_print_table(Data **hash_table) {
     printf("[%d]: ", i);
 
     while (hash_line) {
-      if (hash_line->name != "\0") {
+      if (hash_line->name[0] != '\0') {
         printf("\t| %s", hash_line->name);
       }
       hash_line = hash_line->pNext;
