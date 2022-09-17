5ee4876654ced2d272f9a5244df05c4ebdeaa569
Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
2022-09-17 14:19:44 -0300

    fix: rename bagproblem to knapsack

---
+++ src/grokking/knapsack.h
@@ -0,0 +1,11 @@
+#ifndef _H_KNAPSACK_
+#define _H_KNAPSACK_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void knapsack(int *array, int size);
+
+void pretty_print_array(int *array, int size);
+
+#endif
