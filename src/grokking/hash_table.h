f4776453f9833bab0568cad223d80ce5602aef52
Diego Catalão <81654879+diegocatalao@users.noreply.github.com>
2022-09-26 08:17:02 -0300

    Development (#11)
    * fix: update hash table problem
    
    fix: update hash table problem
    
    * fix: reimplements hash table problem
    
    fix: reimplements hash table problem
    
    * fix: rename bagproblem to knapsack (#9)
    
    Co-authored-by: Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
    
    * Knapsack Problem (#5)
    
    * fix: rename file to currect name
    
    * fix: rename file to currect name
    
    Co-authored-by: Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
    
    * Backtracking n-queen (#10)
    
    * fix: rename bagproblem to knapsack
    
    * feat: start backtracking queen solver
    
    * feat: added n-queen backtrack solver
    
    Co-authored-by: Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
    
    Co-authored-by: Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>

--- src/grokking/hash_table.h
+++ src/grokking/hash_table.h
@@ -13,12 +13,45 @@ typedef struct st_Data {
   struct st_Data *pNext;
 } Data;
 
+/**
+ * @brief Put the values of one string into another.
+ *
+ * @param left {char *} - destination string
+ * @param right {char *} - input string
+ */
+void strswap(char *left, char *right);
+
+/**
+ * @brief Creates a hash (int) from the input string. For each char of the
+ * string, the sum of the modules of the ascii integers is calculated and the
+ * module of the sum is recalculated. (c = mod 11) % MAX_INPUT_SIZE.
+ *
+ * @param keyword {char *} - input string
+ * @return int - returns the hash number
+ */
 int hashc(char *keyword);
 
+/**
+ * @brief Start hash table with a st_Data array.
+ *
+ * @param hash_table {Data **} - array of pointers of Data
+ */
 void init_hash_table(Data **hash_table);
 
+/**
+ * @brief Insert data into the hash table.
+ *
+ * @param hash_table {Data **} - hash table array pointer
+ * @param hash {int} - calculated hash module
+ * @param name {char *} - input name
+ */
 void insert_hash_table(Data **hash_table, int hash, char *name);
 
+/**
+ * @brief Hash Table pretty print
+ *
+ * @param hash_table {Data **} - hash table array pointer
+ */
 void pretty_print_table(Data **hash_table);
 
 #endif
