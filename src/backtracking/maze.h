b042ab0de9b16130c351d569687ab0c8f6e3dd79
Diego Hutter Sobreira Catalão <diego.catalao@gabriel.com.br>
2022-10-08 13:48:01 -0300

    docs: maze solver algorithm

--- src/backtracking/maze.h
+++ src/backtracking/maze.h
@@ -32,16 +32,45 @@ typedef struct {
   char *filepath;
 } Maze;
 
+/**
+ * @brief Allocate maze structure and gets the board information
+ * from filepath.
+ *
+ * @param maze {Maze} - maze struct
+ * @param filepath - maze file to solve
+ */
 void maze_init(Maze **maze, char *filepath);
 
+/**
+ * @brief Dealocate maze struct.
+ *
+ * @param maze {Maze} - maze struct
+ */
 void maze_dealoc(Maze **maze);
 
+/**
+ * @brief Solve maze with backtracking strategy.
+ *
+ * @param maze {Maze} - maze struct
+ * @param x {int} - row position
+ * @param y {int} - column position
+ * @return true {bool} - returns true if gets an solution
+ * @return false {bool} - returns false if not
+ */
 bool maze_solver(Maze **maze, int x, int y);
 
-bool maze_enable_walk(Maze **maze, int x, int y);
-
+/**
+ * @brief Board pretty print.
+ *
+ * @param maze {Maze} - maze struct
+ */
 void maze_printf(Maze **maze);
 
+/**
+ * @brief Maze general information.
+ *
+ * @param maze {Maze} - maze struct
+ */
 void maze_printf_summary(Maze **maze);
 
 #endif
