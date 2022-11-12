04ba0e4414e0e2b2fb059eec94cbf5b180309980
Diego Catalão <diego.catalao@gabriel.com.br>
2022-11-12 12:04:19 -0300

    feat: added boolean counter with division and conquer

---
+++ src/divide-and-conquer/boolean-counter.c
@@ -0,0 +1,26 @@
+#include "boolean-counter.h"
+
+int main(int argc, char **argv) {
+  bool array[] = {true, true, false, false, false, true, false, true, true};
+  bool array_s = sizeof(array) / sizeof(array[0]);
+  int key_count = 0;
+
+  /* Count false elements */
+  key_count = boolean_counter(array, false, 0, array_s);
+  printf("False elements quantity: %d\n", key_count);
+
+  return 0;
+}
+
+int boolean_counter(int *array, bool key, int inf, int sup) {
+  if (sup - inf == 1) {
+    printf("Saída: %d\n", array[inf]);
+    return array[inf] == key ? 1 : 0;
+  }
+
+  int middle = (inf + sup) / 2;
+  int left = boolean_counter(array, key, inf, middle);
+  int right = boolean_counter(array, key, middle, sup);
+
+  return left + right;
+}
---
+++ src/divide-and-conquer/boolean-counter.h
@@ -0,0 +1,13 @@
+#ifndef _H_BOOLEAN_COUNTER_
+#define _H_BOOLEAN_COUNTER_
+
+#include <stdio.h>
+
+#define true 1
+#define false 0
+
+typedef unsigned int bool;
+
+int boolean_counter(int *array, bool key, int inf, int sup);
+
+#endif
